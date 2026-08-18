36df1e6b7ee1eb7fd0931a8c490f8a18ca6cd532
Takshita <takshita.kapur@meritech.co.in>
2026-08-18 10:14:24 +0530

    Updated locators and added test scripts

--- utils/config.py
+++ utils/config.py
@@ -1,18 +1,32 @@
 # CENTRA Configuration
-BASE_URL = ""
-UPLOAD_URL = ""
-CENTRA_USERNAME = ""
-CENTRA_PASSWORD = ""
+BASE_URL = "https://meritech.zapto.org:9115/testingteam/logmask"
+UPDATE_URL = "https://meritech.zapto.org:9115/testingteam/dashboard/1"
+UPLOAD_URL = "https://centra.meritech.co.jp/testingteam/filemanager"
+DEBUG_LOGS_URL = "https://centra.meritech.co.jp/testingteam/debugLogs"
+CENTRA_USERNAME = "Takshita"
+CENTRA_PASSWORD = "Meritech@123"
 
 # Device Configuration
-DEVICE_ID = ""
-DEVICE_SERIAL = ""
+# DEVICE_ID = "9464ad558d4c4e55abd3c7b5035e1b83"
+DEVICE_ID = "e24221d14bad4fbdb90527a062340944"
+DEVICE_SERIAL = "SM-S901E"
+# DEVICE_SERIAL = "RZGYC1QCDHY"
+
+# DEVICE_SERIAL = "R5CT40X4V3M"
 
 #Template Configuration
 IS_SCENARIO = False
 CREATE_TEMPLATE = False
 EDIT_TEMPLATE = False
-TEMPLATE_NAME = ""
-FILE_PREFIX = ""
+# TEMPLATE_NAME = "ASk_ookla"
+TEMPLATE_NAME = "PING"
+FILE_PREFIX = "Automation-Test"
 SAME_AS_TEST_NAME = False
 DETACH_ATTACH = True
+
+#Device Settings
+DEB_TO_UPGRADE = "Sigma-One_26.04.2848_aarch64.deb"
+
+#Update URL
+BASE_URL_API = "https://centra.meritech.co.jp/testingteam/centra.sd/api/"
+UPDATE_URL_API = "https://meritech.zapto.org:9115/sigmapa/centra.sd/api/"
